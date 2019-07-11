4529cb2cd108ca74b576f6ee0df2da94d50ed497
Milu Franz <mfranz@Milus-MacBook-Pro.local>
2019-07-11 10:12:24 -0400

    Add map and change colors

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Usa from "@svg-maps/usa";
+import { CheckboxSVGMap } from "react-svg-map";
+import "react-svg-map/lib/index.css";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CheckboxSVGMap map={Usa} />
     </div>
   );
 }
